61a18ecfd12462e553903f32fbade2265cc6f8e4
Loan <loanbillonnet@gmail.com>
2024-12-31 17:29:00 +0100

    style: :art: update main.ts imports

--- nest-react/frontend/src/main.tsx
+++ nest-react/frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./config/router.tsx";
-
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import router from "./config/router.tsx";
 import store, { persistor } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
